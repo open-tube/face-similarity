b57bab1e0a2a3859f02a0e0699c46469c552bf4d
minhyeoklee <rhodochrosited@gmail.com>
2019-08-07 16:37:31 +0900

    텐서플로우 1 -> 텐서플로우 2.0.0b1

--- model/densenet.py
+++ model/densenet.py
@@ -13,7 +13,7 @@ class TransitionLayer(tf.keras.Model):
 
         self.pool1 = tf.keras.layers.AveragePooling2D(pool_size=2, strides=2, padding="same")
 
-    def call(self, inputs, training):
+    def call(self, inputs, training=False):
         net = self.bn1(inputs, training=training)
         net = tf.nn.relu(net)
         net = self.conv1(net)
@@ -35,7 +35,7 @@ class DenseUnit(tf.keras.Model):
                                             activation=None, kernel_initializer="he_normal",
                                             kernel_regularizer=l2(weight_decay))
 
-    def call(self, inputs, training):
+    def call(self, inputs, training=False):
         net = self.bn1(inputs, training=training)
         net = tf.nn.relu(net)
         net = self.conv1(net)
@@ -49,7 +49,9 @@ class DenseBlock(tf.keras.Model):
     def __init__(self, k, weight_decay, number_of_units, momentum=0.99, epsilon=0.001):
         super(DenseBlock, self).__init__()
         self.number_of_units = number_of_units
-        self.units = self._add_cells([DenseUnit(k, weight_decay=weight_decay, momentum=momentum, epsilon=epsilon) for i in range(number_of_units)])
+        self.units = self._add_cells(
+            [DenseUnit(k, weight_decay=weight_decay, momentum=momentum, epsilon=epsilon) for i in
+             range(number_of_units)])
 
     def _add_cells(self, cells):
         # "Magic" required for keras.Model classes to track all the variables in
@@ -58,7 +60,7 @@ class DenseBlock(tf.keras.Model):
             setattr(self, "cell-%d" % i, c)
         return cells
 
-    def call(self, x, training):
+    def call(self, x, training=False):
         x = self.units[0](x, training=training)
         for i in range(1, int(self.number_of_units)):
             output = self.units[i](x, training=training)
@@ -81,11 +83,13 @@ class DenseNet(tf.keras.Model):
             self.pool1 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding="same")
 
         self.number_of_blocks = len(units_per_block)
-        self.dense_blocks = self._add_cells([DenseBlock(k=k, number_of_units=units_per_block[i], weight_decay=weight_decay,
-                               momentum=momentum, epsilon=epsilon) for i in range(self.number_of_blocks)])
+        self.dense_blocks = self._add_cells(
+            [DenseBlock(k=k, number_of_units=units_per_block[i], weight_decay=weight_decay,
+                        momentum=momentum, epsilon=epsilon) for i in range(self.number_of_blocks)])
 
-        self.transition_layers = self._add_cells([TransitionLayer(theta=theta, depth=k * units_per_block[i], weight_decay=weight_decay,
-                                        momentum=momentum, epsilon=epsilon) for i in range(self.number_of_blocks-1)])
+        self.transition_layers = self._add_cells(
+            [TransitionLayer(theta=theta, depth=k * units_per_block[i], weight_decay=weight_decay,
+                             momentum=momentum, epsilon=epsilon) for i in range(self.number_of_blocks - 1)])
 
         self.glo_avg_pool = tf.keras.layers.GlobalAveragePooling2D()
         self.logits = tf.keras.layers.Dense(units=num_outputs)
@@ -97,8 +101,8 @@ class DenseNet(tf.keras.Model):
             setattr(self, "cell-%d" % i, c)
         return cells
 
-    @tf.contrib.eager.defun
-    def call(self, input, training):
+    # @tf.contrib.eager.defun
+    def call(self, input, training=False):
         """Run the model."""
         net = self.conv1(input)
 
--- src/pre_processing.py
+++ src/pre_processing.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+
 def apply_random_transformations(image, number_of_transformations=2):
     # randomly select two image transformations
     np.random.shuffle(transformations)
@@ -11,33 +12,39 @@ def apply_random_transformations(image, number_of_transformations=2):
 
     return image
 
+
 def random_hue(image):
     return tf.image.random_hue(image, max_delta=0.1)
+
 
 def random_saturation(image):
     return tf.image.random_saturation(image, lower=0.5, upper=1.5)
 
+
 def random_contrast(image):
     return tf.image.random_contrast(image, lower=0.5, upper=1.5)
+
 
 def random_brightness(image):
     return tf.image.random_brightness(image, max_delta=32. / 255.)
 
+
 transformations = np.array([random_contrast, random_brightness, random_saturation, random_hue])
+
 
 def tf_record_parser(record, to_grayscale=False):
     keys_to_features = {
-        "Xi": tf.FixedLenFeature((), tf.string, default_value=""),
-        'Xj': tf.FixedLenFeature([], tf.string),
-        "label": tf.FixedLenFeature((), tf.int64),
-        "height": tf.FixedLenFeature((), tf.int64),
-        "width": tf.FixedLenFeature((), tf.int64)
+        "Xi": tf.io.FixedLenFeature((), tf.string, default_value=""),
+        'Xj': tf.io.FixedLenFeature([], tf.string),
+        "label": tf.io.FixedLenFeature((), tf.int64),
+        "height": tf.io.FixedLenFeature((), tf.int64),
+        "width": tf.io.FixedLenFeature((), tf.int64)
     }
 
-    features = tf.parse_single_example(record, keys_to_features)
+    features = tf.io.parse_single_example(record, keys_to_features)
 
-    Xi = tf.decode_raw(features['Xi'], tf.uint8)
-    Xj = tf.decode_raw(features['Xj'], tf.uint8)
+    Xi = tf.io.decode_raw(features['Xi'], tf.uint8)
+    Xj = tf.io.decode_raw(features['Xj'], tf.uint8)
 
     label = tf.cast(features['label'], tf.int32)
     height = tf.cast(features['height'], tf.int32)
@@ -51,28 +58,29 @@ def tf_record_parser(record, to_grayscale=False):
         Xi = tf.image.rgb_to_grayscale(Xi)
         Xj = tf.image.rgb_to_grayscale(Xj)
 
-    return tf.to_float(Xi) / 255., tf.to_float(Xj) / 255., label
+    return tf.cast(Xi, tf.float32) / 255., tf.cast(Xj, tf.float32) / 255., label
 
 
 def random_resize_and_crop(Xi, Xj, label, crop_size=128):
     input_shape = tf.shape(Xi)[0:2]
     n_channels = tf.shape(Xi)[-1]
-    input_shape_float = tf.to_float(input_shape)
+    input_shape_float = tf.cast(input_shape, tf.float32)
 
-    scale = tf.random_uniform(shape=[1],
+    scale = tf.random.uniform(shape=[1],
                               minval=0.9,
                               maxval=1.1)
 
-    scaled_shape = tf.to_int32(tf.round(input_shape_float * scale))
+    scaled_shape = tf.cast(tf.round(input_shape_float * scale), tf.int32)
 
-    Xi = tf.image.resize_images(Xi, scaled_shape,
+    Xi = tf.image.resize(Xi, scaled_shape,
                                 method=tf.image.ResizeMethod.BILINEAR)
 
-    Xj = tf.image.resize_images(Xj, scaled_shape,
+    Xj = tf.image.resize(Xj, scaled_shape,
                                 method=tf.image.ResizeMethod.BILINEAR)
 
-    Xi = tf.random_crop(Xi, (crop_size, crop_size, n_channels))
-    Xj = tf.random_crop(Xj, (crop_size, crop_size, n_channels))
+    Xi = tf.image.random_crop(Xi, (crop_size, crop_size, n_channels))
+    Xj = tf.image.random_crop(Xj, (crop_size, crop_size, n_channels))
+
     return Xi, Xj, label
 
 
@@ -85,10 +93,12 @@ def random_image_rotation(Xi, Xj, label):
 
     return Xi, Xj, label
 
+
 def random_distortions(Xi, Xj, label):
     Xi = tf.clip_by_value(tf.py_func(apply_random_transformations, [Xi], tf.float32), 0.0, 1.0)
     Xj = tf.clip_by_value(tf.py_func(apply_random_transformations, [Xj], tf.float32), 0.0, 1.0)
     return Xi, Xj, label
+
 
 def random_flip_left_right(Xi, Xj, label):
     Xi = tf.image.random_flip_left_right(Xi)
@@ -104,10 +114,13 @@ def normalizer(Xi, Xj, label):
     Xj = tf.multiply(Xj, 2.0)
     return Xi, Xj, label
 
+
 def central_image_crop(Xi, Xj, label, crop_size=128):
     return tf.image.resize_image_with_crop_or_pad(Xi, target_height=crop_size, target_width=crop_size), \
            tf.image.resize_image_with_crop_or_pad(Xj, target_height=crop_size, target_width=crop_size), \
            label
 
+
 def denormalize(image):
     return tf.add(tf.divide(image, 2), 0.5)
+
